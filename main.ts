c9c0148680241684ec7360271b656aaf41c2374b
silchuk <138850227+silchuk@users.noreply.github.com>
2023-07-07 00:54:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,1 @@
+<iframe style="border-radius:12px" src = "https://open.spotify.com/embed/playlist/6ljLX4tJGH6lYoZ5F5fQeH?utm_source=generator" width = "100%" height = "352" frameBorder = "0" allowfullscreen = "" allow = "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading = "lazy" > </iframe>
